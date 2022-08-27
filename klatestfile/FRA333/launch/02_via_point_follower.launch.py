9d895fdaaed867cd3160154c368bc98942156f20
kla10011 <Kla10011>
2022-08-27 19:22:56 +0700

    del

--- klatestfile/FRA333/launch/02_via_point_follower.launch.py
+++
@@ -1,9 +0,0 @@
-#!usr/bin/python3
-from launch import LaunchDescription
-from launch.actions import ExecuteProcess
-from launch_ros.actions import Node
-
-def generate_launch_description():
-    
-    
-    return LaunchDescription(entity_to_run)
