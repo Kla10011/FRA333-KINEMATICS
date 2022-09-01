7171d34890d2282114ef44aebc311272a478904f
kla10011 <Kla10011>
2022-09-01 15:01:02 +0700

    edit

---
+++ fra333_lab1/lab1_ws/build/lab1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ronnakon/FRA333-KINEMATICS/fra333_lab1/lab1_ws/src/lab1
+# Build directory: /home/ronnakon/FRA333-KINEMATICS/fra333_lab1/lab1_ws/build/lab1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
