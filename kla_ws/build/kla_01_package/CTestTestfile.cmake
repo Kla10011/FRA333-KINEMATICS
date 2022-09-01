c28cfcb5e1f7c0b7445b2ab1c7161004379b714c
kla10011 <Kla10011>
2022-09-01 15:07:23 +0700

    add

--- kla_ws/build/kla_01_package/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student26/ROS2_Directory/kla_ws/src/kla_01_package
-# Build directory: /home/student26/ROS2_Directory/kla_ws/build/kla_01_package
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- kla_ws/src/kla_01_package/src/cpp_node.cpp
+++
@@ -1,8 +0,0 @@
-#include "kla_01_package/cpp_header.hpp"
-
-#include <iostream>
-
-int main() {
-    std::cout << "Hello World!\n";
-    return 0;
-}
