9adc2e3875a3e879f8f8dc776ee47c8803d24914
kla10011 <Kla10011>
2022-09-05 18:13:37 +0700

    test one

--- fra333_lab1_23/fra333_ws/src/fra333_lab1_23/launch/random_walk.launch.py
+++ fra333_lab1_23/fra333_ws/src/fra333_lab1_23/launch/random_walk.launch.py
@@ -13,19 +13,45 @@ def generate_launch_description():
     launch_description.add_action(v_max_launch_arg)
     
     ## Example for adding a node ###
-    node = Node(
-        package='my_package',
-        executable='my_executable',
-        namespace= 'this_namespace',
-        arguments=[v_max],
-        remappings=[
-            ('/topic_1','/topic_a'),
-            ('/topic_2','/topic_b'),
+    turtlesim = Node(
+        package='turtlesim',
+        executable='turtlesim_node',
+        parameters=[
+            {'background_b':100},
+            {'background_g':20},
+            {'background_r':100},
         ]
     )
-    launch_description.add_action(node)
+    linear_noise_generator = Node(
+        package='fra333_lab1_23',
+        executable='noise_generator.py',
+        namespace= '/linear/noise',
+    )
+    angular_noise_generator = Node(
+        package='fra333_lab1_23',
+        executable='noise_generator.py',
+        namespace= '/angular/noise',
+    )
+    velocity_mux = Node(
+        package='fra333_lab1_23',
+        executable='velocity_mux.py',
+        namespace= 'velocity_mux',
+    )
 
-    ### Example for execute a shell command in python script ###
+    launch_description.add_action(turtlesim,linear_noise_generator,angular_noise_generator,velocity_mux)
+    # node = Node(
+    #     package='fra333_lab1_23',
+    #     executable='my_executable',
+    #     namespace= 'this_namespace',
+    #     arguments=[v_max],
+    #     remappings=[
+    #         ('/topic_1','/topic_a'),
+    #         ('/topic_2','/topic_b'),
+    #     ]
+    # )
+    # launch_description.add_action(node)
+
+    # ## Example for execute a shell command in python script ###
     # vx = 1.0
     # wz = 1.0
     # pub_cmd_vel = ExecuteProcess(
@@ -33,8 +59,6 @@ def generate_launch_description():
     #     shell=True
     # )
     # launch_description.add_action(pub_cmd_vel)
-
-    
-    return launch_description
+    return launch_description()
 
     
--- fra333_lab1_23/fra333_ws/src/fra333_lab1_23/scripts/velocity_mux.py
+++ fra333_lab1_23/fra333_ws/src/fra333_lab1_23/scripts/velocity_mux.py
@@ -13,34 +13,39 @@ class VelocityMux(Node):
     def __init__(self):
         super().__init__('velocity_multiplexer')
         # get the rate from argument or default
-        if sys.argv[1]:
+        if len(sys.argv)>2:
             self.rate = float(sys.argv[1])
         else:
             self.rate = 5.0
-        # add codes here
+        # add codes topic here
         self.cmd_vel = self.create_publisher(Twist,'/turtle/cmd_vel',10)
+        self.timer = self.create_timer(1/self.rate,self.timer_callback)  
+        # add codes sub here
         self.linear_sub = self.create_subscription(Float64,'/linear/noise',self.linear_vel_sub_callback,10)
         self.angular_sub = self.create_subscription(Float64,'/angular/noise',self.angular_vel_sub_callback,10)
-
         # additional attributes
-        self.cmd_vel = Twist()
+        self.cmd = Twist()
         self.get_logger().info(f'Starting {self.get_name()}')
 
     def linear_vel_sub_callback(self,msg:Float64):
-        # remove pass and add codes here
-        pass
+        self.cmd.linear.x = msg
     
     def angular_vel_sub_callback(self,msg:Float64):
-        # remove pass and add codes here
-        pass
+        self.cmd.angular.z = msg
+
     
     def timer_callback(self):
-        # remove pass and add codes here
-        pass
+        self.cmd_vel.publish(self.cmd)
+
+       
 
 def main(args=None):
-    # remove pass and add codes here
-    pass
+    rclpy.init(args=args)
+    velo_mux = VelocityMux()
+    print("hahahaha")
+    rclpy.spin(velo_mux)
+    velo_mux.destroy_node()
+    rclpy.shutdown()
 
 if __name__=='__main__':
     main()
