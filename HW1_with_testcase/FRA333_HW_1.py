f2eeb9d10edfd32a56eb627617314e527a0d6212
kla10011 <klaza565@gmail.com>
2022-09-16 00:09:29 +0700

    edit

--- HW1_with_testcase/FRA333_HW_1.py
+++ HW1_with_testcase/FRA333_HW_1.py
@@ -11,23 +11,28 @@ class MyBeeBot(BeeBot):
         super().__init__(a_i)
         self.a_i = a_i
         self.d = 1.0
-    def numbers_to_strings(argument):
+        self.direction = 0 # intial degrees
+    def stash_direction(self,degrees):
+        self.direction += degrees
+        return self.direction
+    def numbers_to_action(argument):
+        A=MyBeeBot(BeeBot) 
         switcher = {
-            0: "unknow",
-            1: "unknow",
-            2: "unknow",
-            3: int(60),
-            4: int(-60),
+            0: "stop",
+            1: "forward",
+            2: "backward",
+            3: A.stash_direction(60),
+            4: A.stash_direction(-60),
         }
         return switcher.get(argument, "nothing")
-    def numbers_to_d(argument):
+    def numbers_to_direction(argument): #affect ot a_i,a_j
         switcher = {
-            0: "unknow",
-            1: "unknow",
-            2: "unknow",
-            3: int(60),
-            4: int(-60),
-            5: int(-60),
+            0: [1,1],
+            1: [0,1],
+            2: [-1,0],
+            3: [-1,-1],
+            4: [0,-1],
+            5: [1,0],
         }
         return switcher.get(argument, "nothing")
     # a_i is initial position
@@ -39,10 +44,12 @@ class MyBeeBot(BeeBot):
     # com is command {'0'->stop, '1'->forward, '2'->backward, '3'->turn right, '4'->turn left}
     # W is wall
     # add your code here
-        state = self.a_i
-        direction = 0 # intial degrees
+        trackBB = MyBeeBot(BeeBot) 
+        trackBB.state = self.a_i
+        trackBB.direc = self.direction # intial degrees
         for i in com:
             com_state = int(i)
+            action = trackBB.numbers_to_action(i)
             
 
             wall = np.concatenate((list(map(lambda x : [x],W[0])),list(map(lambda y : [y],W[1]))),1) # create wall
