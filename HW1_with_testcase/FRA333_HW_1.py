85d638f6df2533e85284b5519d29cee72a0b53d4
Kla10011 <klaza565@gmail.com>
2022-09-15 18:23:41 +0700

    edit

--- HW1_with_testcase/FRA333_HW_1.py
+++ HW1_with_testcase/FRA333_HW_1.py
@@ -11,6 +11,25 @@ class MyBeeBot(BeeBot):
         super().__init__(a_i)
         self.a_i = a_i
         self.d = 1.0
+    def numbers_to_strings(argument):
+        switcher = {
+            0: "unknow",
+            1: "unknow",
+            2: "unknow",
+            3: int(60),
+            4: int(-60),
+        }
+        return switcher.get(argument, "nothing")
+    def numbers_to_d(argument):
+        switcher = {
+            0: "unknow",
+            1: "unknow",
+            2: "unknow",
+            3: int(60),
+            4: int(-60),
+            5: int(-60),
+        }
+        return switcher.get(argument, "nothing")
     # a_i is initial position
     # c is command {'0'->stop, '1'->forward, '2'->backward, '3'->turn right, '4'->turn left}
     # o is obstacle
@@ -24,8 +43,8 @@ class MyBeeBot(BeeBot):
         direction = 0 # intial degrees
         for i in com:
             com_state = int(i)
-            if i in [3,4]:
-                direction
+            
+
             wall = np.concatenate((list(map(lambda x : [x],W[0])),list(map(lambda y : [y],W[1]))),1) # create wall
             if state in wall: # condition wall
                 pass
