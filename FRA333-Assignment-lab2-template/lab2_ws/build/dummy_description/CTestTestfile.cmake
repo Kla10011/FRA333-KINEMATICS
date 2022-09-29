76938ab898a915c0f4bca2e0ae7a53ed59c10c4c
Kla10011 <klaza565@gmail.com>
2022-09-29 14:19:04 +0700

    colcon

---
+++ FRA333-Assignment-lab2-template/lab2_ws/build/dummy_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ronnakon/FRA333-KINEMATICS/FRA333-Assignment-lab2-template/lab2_ws/src/dummy_description
+# Build directory: /home/ronnakon/FRA333-KINEMATICS/FRA333-Assignment-lab2-template/lab2_ws/build/dummy_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ FRA333-Assignment-lab2-template/lab2_ws/install/dummy_description/share/dummy_description/cmake/dummy_descriptionConfig-version.cmake
@@ -0,0 +1,1 @@
+/home/ronnakon/FRA333-KINEMATICS/FRA333-Assignment-lab2-template/lab2_ws/build/dummy_description/ament_cmake_core/dummy_descriptionConfig-version.cmake
